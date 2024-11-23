84f8df070ac18ce62335a028d6297dcad496ab58
Jo <134626490+Jcait@users.noreply.github.com>
2024-11-23 14:23:10 +0000

    add func to fetch api

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -5,6 +5,21 @@ function Searchbar() {
   // The auto complete components requires an Array of Obejcts, when clearing the state use an empty array
   const testItems = [{ name: "Bark" }, { name: "Meow" }, { name: "Quack" }];
   const [suggestions, setSuggestions] = useState([]);
+
+  const fetchDogSuggests = async () => {
+    try {
+      const breeds = await fetch("https://dog.ceo/api/breeds/list/all").then(
+        (response) => response.json()
+      );
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleSearch = () => {
+    fetchDogSuggests();
+  };
+
   return (
     <>
       <div className="search-container">
