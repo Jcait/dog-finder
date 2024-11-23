5f93306bc489a4ad22eb6b70364aa2da5707bffa
Jo <134626490+Jcait@users.noreply.github.com>
2024-11-23 14:40:37 +0000

    move fetch to hook
    changes from being called constantly to just once

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,27 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ReactSearchAutocomplete } from "react-search-autocomplete";
 
 function Searchbar() {
   // The auto complete components requires an Array of Obejcts, when clearing the state use an empty array
   const [suggestions, setSuggestions] = useState([]);
 
-  const fetchDogSuggests = async () => {
-    try {
-      const breeds = await fetch("https://dog.ceo/api/breeds/list/all").then(
-        (response) => response.json()
-      );
-      const autocomplete = Object.keys(breeds.message).map((breed) =>
-        Object.assign({ name: breed })
-      );
-      setSuggestions(autocomplete);
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
-
-  //   This fires on every input, like onChange
-  const handleSearch = () => {
+  useEffect(() => {
+    const fetchDogSuggests = async () => {
+      try {
+        const breeds = await fetch("https://dog.ceo/api/breeds/list/all").then(
+          (response) => response.json()
+        );
+        // Changes the JSON response to an array of objects
+        const autocomplete = Object.keys(breeds.message).map((breed) =>
+          Object.assign({ name: breed })
+        );
+        setSuggestions(autocomplete);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
     fetchDogSuggests();
+  }, []);
+
+  //   This fires on every input, like onChange, string tracks the searchbars value
+  //   will be used to update state to fetch image
+  const handleSearch = (string) => {
+    console.log(string);
   };
 
   return (
