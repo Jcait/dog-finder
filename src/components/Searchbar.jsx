d47953f252260a2ce67efb594615fd859acdc065
Jo <134626490+Jcait@users.noreply.github.com>
2024-11-23 14:01:11 +0000

    add state and testitems

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { ReactSearchAutocomplete } from "react-search-autocomplete";
 
 function Searchbar() {
+  // The auto complete components requires an Array of Obejcts, when clearing the state use an empty array
+  const testItems = [{ name: "Bark" }, { name: "Meow" }, { name: "Quack" }];
+  const [suggestions, setSuggestions] = useState([]);
   return (
     <>
       <div className="search-container">
         <h4>Find a dog</h4>
-        <ReactSearchAutocomplete className="searchbar" />
+        <ReactSearchAutocomplete items={testItems} className="searchbar" />
       </div>
     </>
   );
